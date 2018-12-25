4079365f89073bc4cb020511868685c13e1d532b
himustofa <mustofa2008@gmail.com>
2018-12-25 15:42:36 +0600

    Modify constant key and home activity move to activity package

--- app/src/main/java/com/apps/newsviews/retrofit/RetrofitClient.java
+++ app/src/main/java/com/apps/newsviews/retrofit/RetrofitClient.java
@@ -1,19 +1,18 @@
 package com.apps.newsviews.retrofit;
 
+import com.apps.newsviews.utility.ConstantKey;
+
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    //https://medium.com/@sraju432/news-application-using-retrofit-2-be8d052c7aca
-    //private static final String BASE_URL = "https://newsapi.org/v2/everything?q=bitcoin&from=2018-11-25&sortBy=publishedAt&apiKey=9688fe5fdd5148c4bcee81fcf94e1837";
-    private static final String BASE_URL = "https://newsapi.org/v2/";
     private static RetrofitClient mInstance;
     private static Retrofit retrofit;
 
     private RetrofitClient() {
         retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL) //"https://api.github.com"
+                .baseUrl(ConstantKey.BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
     }
