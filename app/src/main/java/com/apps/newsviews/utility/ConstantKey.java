a9b1c14cabeae43375d3d883984a2bf9d66945e9
himustofa <mustofa2008@gmail.com>
2018-12-27 13:24:51 +0600

    commet it

--- app/src/main/java/com/apps/newsviews/utility/ConstantKey.java
+++ app/src/main/java/com/apps/newsviews/utility/ConstantKey.java
@@ -2,19 +2,18 @@ package com.apps.newsviews.utility;
 
 public class ConstantKey {
 
-    public final static String USER_LOGIN_KEY = "userLoginInfo";
-    public final static String USER_IS_LOGGED_KEY = "isUserLoggedIn";
     public final static String WEB_URL = "webUrl";
     public final static String NUMBER_API_RESULT_KEY = "resultKey";
 
     public final static String BASE_URL = "https://newsapi.org/v2/";
-    //public final static String NEWS_API = "9688fe5fdd5148c4bcee81fcf94e1837"; //https://newsapi.org/
-    public final static String COIN = "bitcoin";
-    public final static String DATE = "2018-11-26";
-    public final static String SORT = "publishedAt";
-    public final static String API = "9688fe5fdd5148c4bcee81fcf94e1837";
+    public final static String COIN = "Apple";
+    public final static String DATE = "2018-12-27";
+    public final static String SORT = "popularity";
+    public final static String API = "9688fe5fdd5148c4bcee81fcf94e1837"; //https://newsapi.org/
     public final static String NUMBER_API = "http://numbersapi.com/number/type"; //http://numbersapi.com/42/trivia
 
+    //@GET("everything")
+    //Call<ResponseModel> getNews(@Query("q") String q, @Query("from") String from, @Query("sortBy") String sortBy, @Query("apiKey") String apiKey);
     //private static final String BASE_URL = "https://newsapi.org/v2/everything?q=bitcoin&from=2018-11-26&sortBy=publishedAt&apiKey=9688fe5fdd5148c4bcee81fcf94e1837";
 
 }
